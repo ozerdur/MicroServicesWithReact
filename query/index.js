95e66a06b332c13a15a4e2484f8a5572b7e3e39c
TURKCELL\TTODURMAZ <ozer.durmaz@turkcell.com.tr>
2021-05-08 00:06:08 +0300

    first commit

---
+++ query/index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+  handleEvent(type, data);
+  res.send({});
+});
+
+function handleEvent(type, data) {
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  } else if (type === "CommentCreated") {
+    const { id, content, postId, status } = data;
+    const post = posts[postId];
+    post.comments.push({ id, content, status });
+  } else if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data;
+    console.log(data);
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => {
+      return comment.id === id;
+    });
+    comment.status = status;
+    comment.content = content;
+  }
+}
+
+app.listen(4002, async () => {
+  console.log("Listening on 4002");
+  const res = await axios.get("http://localhost:4005/events");
+
+  console.log(res.data);
+  for (let event of res.data) {
+    console.log("Handle Event", event);
+    handleEvent(event.type, event.data);
+  }
+});
---
+++ readMe.md
@@ -0,0 +1,19 @@
+Create client 
+    mkdir client
+    cd client
+    npx create-react-app client
+    npm i axios
+
+Create posts 
+    mkdir posts
+    cd posts
+    npm init -y
+    npm install express cors axios nodemon
+
+
+
+Create comments 
+    mkdir comments
+    cd comments
+    npm init -y
+    npm install express cors axios nodemon
